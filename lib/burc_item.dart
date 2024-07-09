cf05b29ecdd0631f81100fdf490384dde4b95a53
efeeren12 <elibolefe12@gmail.com>
2024-07-09 09:58:01 +0300

    a

--- lib/burc_item.dart
+++ lib/burc_item.dart
@@ -20,25 +20,12 @@ class BurcItem extends StatelessWidget {
               onTap:
               () {
                 Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          BurcDetay(secilenBurc: listelenenBurc),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = Offset(1.0, 0.0);
-                        var end = Offset.zero;
-                        var curve = Curves.ease;
-
-                        var tween = Tween(begin: begin, end: end)
-                            .chain(CurveTween(curve: curve));
-
-                        return SlideTransition(
-                          position: animation.drive(tween),
-                          child: child,
-                        );
-                      },
-                    ));
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        BurcDetay(secilenBurc: listelenenBurc),
+                  ),
+                );
               };
               Navigator.of(context).push(
                 MaterialPageRoute(
